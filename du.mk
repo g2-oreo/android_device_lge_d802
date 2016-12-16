5c496cb9b02205b061460d270059148f58f8b3ec
nasty007 <lazic.josip@gmail.com>
2016-12-16 17:17:23 +0100

    d802: Remove enhanced NFC feature

--- du.mk
+++ du.mk
@@ -8,9 +8,6 @@ TARGET_SCREEN_WIDTH := 1080
 # Inherit some common du stuff.
 $(call inherit-product, vendor/du/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/lge/d802/device.mk)
 
